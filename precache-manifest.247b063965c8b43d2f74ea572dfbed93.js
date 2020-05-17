20b3f81b4a200e58651717a46a12c57f0ab6143e
Sijia Xiao <lenaxiao@uw.edu>
2020-05-17 13:09:04 -0700

    Updates

---
+++ precache-manifest.247b063965c8b43d2f74ea572dfbed93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfb47842bb903873c79793fcadc9bf7f",
+    "url": "/covid19-visualizer/index.html"
+  },
+  {
+    "revision": "dda48a76cc1f3d0603a8",
+    "url": "/covid19-visualizer/static/css/main.2f13df48.chunk.css"
+  },
+  {
+    "revision": "80cb8107b1cefcffe842",
+    "url": "/covid19-visualizer/static/js/2.2e356a93.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-visualizer/static/js/2.2e356a93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dda48a76cc1f3d0603a8",
+    "url": "/covid19-visualizer/static/js/main.66c058d4.chunk.js"
+  },
+  {
+    "revision": "cd2f2b52cfaea822351d",
+    "url": "/covid19-visualizer/static/js/runtime-main.7abe3600.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-visualizer/static/media/image.d7265326.png"
+  }
+]);
